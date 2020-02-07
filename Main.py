01406eebd239e4009c96423fe0f6bad5720745c6
RyanSMac <ryanmcconville93@gmail.com>
2020-02-07 17:29:48 +0000

    Add Occupied

--- Main.py
+++ Main.py
@@ -17,6 +17,7 @@ board_x = 10
 board_y = 10
 
 board = [[0] * board_x for i in range(board_y)]
+board_occupied = [[0] * board_x for i in range(board_y)]
 
 print(board)
 
@@ -26,12 +27,18 @@ player1 = Units.set_up_rebel()
 player2 = Units.set_up_imperial()
 
 player1[0][0].pos = board[2][0].pos
+board_occupied[2][0] = 1
 player1[1][0].pos = board[4][0].pos
+board_occupied[4][0] = 1
 player1[2][0].pos = board[7][0].pos
+board_occupied[7][0] = 1
 
 player2[0][0].pos = board[2][9].pos
+board_occupied[2][9] = 1
 player2[1][0].pos = board[4][9].pos
+board_occupied[4][9] = 1
 player2[2][0].pos = board[7][9].pos
+board_occupied[7][9] = 1
 
 
 def draw():
